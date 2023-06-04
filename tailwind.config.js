df0fe4a1790253652327812c27818dfb7f0b0726
wych(witch) <wych@wychwit.ch>
2023-06-04 18:45:42 -0500

    aaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#788ae3',
+        rose: '#EFB3DB',
+        garden: '#8fd482',
+        magic: '#A495DF',
+        whitish: '#F5F6FE',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    require('flowbite/plugin')
+  ],
 }
