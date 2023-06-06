dee3f8eef203128c81fbc31a49eed8a67bed9b5a
wych(witch) <wych@wychwit.ch>
2023-06-06 01:45:45 -0500

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {
+      listStyleImage: {
+        odd: 'url("/img/bullet-odd.png")',
+        even: 'url("/img/bullet-even.png")',
+      },
       colors: {
         blue: '#788ae3',
         rose: '#EFB3DB',
